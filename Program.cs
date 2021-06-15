7ba44d1c01a34d15f202f75ea4bdd85e93949d36
Xyber Pastoril <cyptess6274@pm.me>
2021-06-15 13:53:21 +0800

    Revert "Add MessageBox Prompt when exiting program while there's an active routine running"

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
-using BeTimelyProject;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -19,39 +17,7 @@ namespace ProductivBoostProject
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
-            Engine e = new Engine();
-            e.MainForm.FormClosing += new FormClosingEventHandler(e.MainForm_FormClosing);
-
-            Application.Run(e.MainForm);
-        }
-
-        
-    }
-
-    public class Engine
-    {
-        public MainForm MainForm;
-        public Engine()
-        {
-            this.MainForm = new MainForm();
-        }
-
-        public void MainForm_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            if (MainForm.CurrentRoutineTasks.Count != 0)
-            {
-                DialogResult result = MessageBox.Show(
-                    "You currently have an active routine. Are you sure to stop and exit program?", "",
-                    MessageBoxButtons.YesNo,
-                    MessageBoxIcon.Question);
-
-                e.Cancel = (result == DialogResult.No);
-            }
-            else
-            {
-                e.Cancel = false;
-            }
+            Application.Run(new Form1());
         }
     }
 }
